38b0c917038e0a061ed9ecb4e61ce78cbc52e911
Matt Bertino <mattbertino@gmail.com>
2014-03-05 16:33:45 -0500

    the gallery views are populating and rolling

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -5,7 +5,7 @@ var AboutView = Backbone.View.extend({
 	aboutTemplate: _.template($('#about-template').text()),
 
 	initialize: function(){
-		$('.main-area').html( this.el );
+		$('.js-main-area').html( this.el );
 		this.render();
 
 	},
@@ -17,17 +17,53 @@ var AboutView = Backbone.View.extend({
 });
 
 var GalleryWinnerView = Backbone.View.extend({
-	className: 'artist-small col-md-4',
+	className: 'artist-small-container js-artist-small-container col-md-3',
 
 	aboutTemplate: _.template($('#artist-small-template').text()),
 
+	events: {
+		"click .js-artist-small-click": "expandedView"
+	},
+
 	initialize: function(){
-		$('.main-selection-area').append( this.el );
+		$('.js-main-selection-area').append( this.el );
+		this.render()
+	},
+
+	render: function(){
+		this.$el.append(this.aboutTemplate({model: this.model}))
+	},
+
+	expandedView: function () {
+		console.log('clicked')
+		new ExpandedView({model: this.model})
+	}
+
+});
+
+var ExpandedView = Backbone.View.extend({
+	className: 'artist-expanded-container  col-lg-12',
+
+	aboutTemplate: _.template($('#artist-expanded-template').text()),
+
+	initialize: function(){
+		$('.js-expanded-container').append( this.el );
 		this.render()
 	},
 
 	render: function(){
 		this.$el.append(this.aboutTemplate({model: this.model}))
 	}
+})
 
-});
+
+
+
+
+
+
+
+
+
+
+
