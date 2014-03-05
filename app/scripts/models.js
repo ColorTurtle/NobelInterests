7c9c9aeaa6c4a9cffa0092bf5041d694010177e4
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-03-05 14:05:16 -0500

    API rixed.

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -4,11 +4,11 @@ var WinnerCollection = Backbone.Collection.extend({
 	model: Winners,
 
 	// API call by genre
-	url: 'http://developer.echonest.com/api/v4/genre/artists?api_key=RMUWZHZJZCSLALMHF&format=jsonp&results=15&bucket=hotttnesss&bucket=discovery&bucket=genre&bucket=artist_location&bucket=images&bucket=years_active&bucket=familiarity&callback=MyFunc&name=jazz',
+	url: 'http://developer.echonest.com/api/v4/artist/search?api_key=RMUWZHZJZCSLALMHF&format=jsonp&results=15&bucket=hotttnesss&bucket=discovery&bucket=genre&bucket=artist_location&bucket=images&bucket=familiarity&callback=?',
 	// Swiching api to artist search with my own public key
 	// url: 'http://developer.echonest.com/api/v4/artist/search?api_key=MDDCRMKPKYI3LCHYF&format=json&description=heavy&mood=sad&style=jazz&results=1',
-
-	parse: function(response){
-		return response.artists;
+	// Not able to get the data into the collection
+	parse: function(data){
+		return data.response.artists;
 	}
 });
